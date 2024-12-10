575cdb42f83365019321e182e0b329436550975a
mochoping <hodolskm@gmail.com>
2024-12-10 17:49:38 +0900

    Service Run / try-catch 활용

--- src/com/kh/practiceEx/filePre/FileService.java
+++ src/com/kh/practiceEx/filePre/FileService.java
@@ -1,37 +1,96 @@
 package com.kh.practiceEx.filePre;
 
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
 
 public class FileService {
 
     // File FileWriter 이용해서 바탕화면에 food.docs 생성하기
     // try-catch
 
-    /** void createFile(String fileName)
+    /**
+     * void createFile(String fileName)
      *
      * @param content
      */
     public void createFile(String content) {
         // 사용자 바탕화면 경로 가져오기
-        String path = System.getProperty("user.home")+ "/Desktop/";
+        String path = System.getProperty("user.home") + "/Desktop/";
         String filename = "food.docs";
 
 
         File file = new File(path + filename); //파일경로와 파일명 가져오기
 
         // try() 안에 FileWriter 나 FileREader BufferedReader 를 작성하면 close는 생략 가능
-        try(FileWriter fw = new FileWriter(file)){
+        try (FileWriter fw = new FileWriter(file)) {
             fw.write(content); // 내용 작성하기
             System.out.println("File Created");
         } catch (Exception e) {
             System.out.println(e.getMessage() + "파일 생성중 문제가 발생했습니다.");
         }
+    }
 
+    /**
+     * 파일 읽기 기능 설정
+     */
+    public void readFile() {
 
+        String path = System.getProperty("user.home") + "/Desktop/";
+        String filename = "food.docs";
 
+        File file = new File(path + filename);
+        if (file.exists()) {
+            try {
+                FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr);
+                System.out.println("File Read : " + file);
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage() + "파일을 읽는중 오류가 발생했습니다.");
+            }
+        } else {
+            System.out.println("File Not Found");
+        }
 
 
     }
 
+
+    // todo : food.docs 파일에 이어쓰기 기능 설정해서 이어서 음식 작성
+    // 줄바꿈 - 햄버거 줄바꿈 - 돈까스 줄바꿈 - 쌀국수 줄바굼 - 카레
+
+    /** 파일 이어쓰기 기능 설정
+     * void appendToFile(String content)
+     * @param content 파일내용
+     */
+    public void appendToFile(String content) {
+        String path = System.getProperty("user.home") + "/Desktop/";
+        String filename = "food.docs";
+        File file = new File(path + filename);
+        if (file.exists()) {
+            try (FileWriter fw = new FileWriter(file, true);) {
+            fw.write(content);
+            } catch (Exception e) {
+            System.out.println(e.getMessage()+"문제가 발생했습니다. ");
+            } finally {
+            System.out.println(  content + "이어쓰기가 완료되었습니다.");
+            }
+        } else {
+            System.out.println("File Not Found");
+        }
+
+/*try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+                FileWriter fw = new FileWriter(file, true);
+                fw.write(content);
+                fw.close();
+                System.out.println( content +"\n 이어쓰기가 완료되었습니다.");
+        } catch (IOException e) {
+            System.out.println(e.getMessage()+"문제가 발생했습니다. ");
+        }*/
+    }
 }
--- src/com/kh/practiceEx/filePre/FileServiceRun.java
+++ src/com/kh/practiceEx/filePre/FileServiceRun.java
@@ -5,7 +5,10 @@ public class FileServiceRun {
         FileService fileService = new FileService();
 
         String content = "맛있는 음식 목록 : \n - 피자 \n - 치킨 \n - 초밥 \n - 파스타";
-
+        String content2 = "\n - 햄버거 \n - 돈까스 \n - 쌀국수 \n - 카레";
         fileService.createFile(content);
+        fileService.readFile();
+        fileService.appendToFile(content2);
+        fileService.readFile();
     }
 }
